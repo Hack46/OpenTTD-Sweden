57f1bde2b294e0eea2497a34b5068e633db05bc3
Matthias Erll <matthias@precisdigital.com>
2015-03-28 20:48:03 +0100

    Improved x scaling.

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ import os
 import png
 
 
-XSCALE = 766
-YSCALE = 2048
-OUTFILE = 'test_3.png'
+Y_RANGE = 2048
+OUTFILE = 'test.png'
 
 
 def process_map(file_name):
@@ -28,13 +27,21 @@ def process_files(base_dir):
 if __name__ == '__main__':
     # Download and unzip file from 'http://www.myndighetsdata.se/download/lantmateriet-hdb-50m.zip' and set path below.
     df = process_files('/Users/matthias/Downloads/lantmateriet-hdb-50m')
-    df['xcat'] = pd.cut(df.x, XSCALE, labels=range(XSCALE))
-    df['ycat'] = pd.cut(df.y, YSCALE, labels=range(YSCALE))
+    print('Scaling down...')
+    x_max = df.x.max()
+    x_min = df.x.min()
+    y_max = df.y.max()
+    y_min = df.y.min()
+    scale = Y_RANGE / (y_max - y_min)
+    x_range = int(np.round(scale * (x_max - x_min)))
+    df['xcat'] = pd.cut(df.x, x_range, labels=range(x_range))
+    df['ycat'] = pd.cut(df.y, Y_RANGE, labels=range(Y_RANGE))
     group = df.groupby(['xcat', 'ycat'])
     df1 = pd.DataFrame(group.z.mean())
+    df1.to_msgpack('new.msgpack')
     max_z = df1.z.max()
-    df1['new_z'] = np.round(df1.z / max_z * 255)
+    df1['new_z'] = np.round(df1.z / max_z * 150)
     dz = df1.new_z.unstack().fillna(0).transpose()
-    w = png.Writer(XSCALE, YSCALE, greyscale=True)
+    w = png.Writer(x_range, Y_RANGE, greyscale=True)
     with open(OUTFILE, 'wb') as f:
         w.write(f, reversed(dz.values.tolist()))
